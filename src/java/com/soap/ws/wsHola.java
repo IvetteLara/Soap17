bae22eee7616949a2cca9044b6200bb6bfd189e3
IvetteLara <ivette.lara2012@gmail.com>
2017-08-13 13:59:31 -0600

    Commit Soap Inicial

---
+++ src/java/com/soap/ws/wsHola.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.soap.ws;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author java
+ */
+@WebService(serviceName = "wsHola")
+public class wsHola {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hola")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hola " + txt + " !";
+    }
+}
